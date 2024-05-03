bfabfb3c66af3f977fa5882411ea0b5c6b835a5a
David Andai <andaidavid97@gmail.com>
2024-05-03 18:37:14 +0300

    dementia_alcohol_shiny

---
+++ dementia/README.md
@@ -0,0 +1,23 @@
+# Python Shiny Dashboard
+The project demonstrates a process from data cleaning to shiny dashboard
+- starting from dementia notebook. Data is imported and restructured
+- demo plots are made in the notbook
+- this is then transfered to shiny app to render in the front end
+
+# Installation
+
+To install the required libraries, type
+
+    pip install -r requirements.txt
+
+# Run Server
+
+After installing, open command prompt and run the shiny application by command
+
+    shiny run app.py
+
+# The Dashboard
+
+Now the shiny server is running. Use the following URL to open dashboard in browser.
+
+    http://localhost:53027/
---
+++ dementia/app.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from shiny import App, Inputs, Outputs, Session, render, ui
+from shinywidgets import output_widget, render_widget
+import plotly.graph_objs as go
+
+# data sets
+from africa_list import african_countries 
+data = pd.read_csv('data\combined_df')
+
+
+app_ui = ui.page_fluid(
+    ui.input_select("select_country","select",choices= african_countries,selected=None),
+    ui.output_text("value"),
+    output_widget("plot")
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    @render.text()
+    def value():
+        return "Your country: " + str(input.select_country())
+    
+
+    @render_widget()
+    def plot():
+
+        df = data[data['Entity'] == str(input.select_country())]
+
+        fig = go.Figure()
+
+        for variable in ['alcohol_consumption', 'Cases_100,000_people']:
+            fig.add_trace(go.Scatter(x=df['Year'], 
+                                     y=df[variable], 
+                                     mode='lines', 
+                                     name=f"{variable}"))
+        return fig
+
+app = App(app_ui, server)
